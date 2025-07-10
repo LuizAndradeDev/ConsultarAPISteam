48e5e5dfc1ffa6c0c59ea46e077373a04e4165d7
Luiz Souza <luizsouzandrade.dev@gmail.com>
2025-07-10 10:22:32 -0300

    Primeiro commi, lançando projeto

---
+++ src/main/java/com/br/luizdev/ConsultarAPISteam/ConsultarApiSteamApplication.java
@@ -0,0 +1,13 @@
+package com.br.luizdev.ConsultarAPISteam;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConsultarApiSteamApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConsultarApiSteamApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/br/luizdev/ConsultarAPISteam/ConsultarApiSteamApplicationTests.java
@@ -0,0 +1,13 @@
+package com.br.luizdev.ConsultarAPISteam;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConsultarApiSteamApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
